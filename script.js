f31beeacb3939fa58209f4d8ac5f2104a53ca7fd
Tran Xuan Thang <tranthang@outlook.com>
2019-05-19 00:21:42 +0700

    First commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var add;
+
+const loadWebAssembly = fileName => fetch(fileName)
+  .then(response => response.arrayBuffer())
+  .then(bits => WebAssembly.compile(bits))
+  .then(module => {
+    return new WebAssembly.Instance(module)
+  });
+
+loadWebAssembly("add.wasm")
+  .then(instance => {
+    add = instance.exports.add;
+    console.log('Ready!');
+  });
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("addBtn").addEventListener("click", () => {
+    let numA = document.getElementById("numA").value;
+    let numB = document.getElementById("numB").value;
+    document.getElementById("result").innerText = add(numA, numB);
+  });
+});
